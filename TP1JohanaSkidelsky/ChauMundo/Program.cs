0fbec6898f9d6421b583d6769b4f5b240a54539f
Dinorah <dinorahskidelsky@gmail.com>
2022-10-20 13:00:12 +0200

    Add project files.

---
+++ TP1JohanaSkidelsky/ChauMundo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChauMundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+            Console.WriteLine("Chau Mundo");
+            Console.Read();
+        }
+    }
+}
